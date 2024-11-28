6b041e69f547b0d840d6a2306ef2f825aa375ada
Luis Osio Chico <72286405+LOsioChico@users.noreply.github.com>
2024-11-28 15:40:25 -0500

    refactor: Improve error handling in pokemonResponseMapper

--- src/utils/pokemonResponseMapper.ts
+++ src/utils/pokemonResponseMapper.ts
@@ -9,36 +9,43 @@ interface MapperOptions {
 export const pokemonResponseMapper = async (
   response: any,
   options: MapperOptions = { includeAbilities: false },
-): Promise<Pokemon> => {
-  const officialArtwork = response.sprites.other["official-artwork"].front_default;
-  const types = response.types.map((type: { type: { name: string } }) => type.type.name);
+): Promise<Pokemon | null> => {
+  try {
+    const officialArtwork = response?.sprites?.other["official-artwork"]?.front_default;
+    const types = response?.types?.map((type: { type: { name: string } }) => type.type.name);
 
-  let abilities = [];
-  if (options.includeAbilities) {
-    const abilitiesPromises = response.abilities.map(async (ability: { ability: { name: string; url: string } }) => {
-      const { data } = await pokeApi.get(ability.ability.url);
+    let abilities = [];
+    if (options.includeAbilities) {
+      const abilitiesPromises = response?.abilities?.map(
+        async (ability: { ability: { name: string; url: string } }) => {
+          const { data } = await pokeApi.get(ability.ability.url);
 
-      const nameEs = data.names.find((name: any) => name.language.name === "es")?.name || ability.ability.name;
-      const descriptionEs =
-        data.flavor_text_entries.find((entry: any) => entry.language.name === "es")?.flavor_text?.replace(/\n/g, " ") ||
-        "";
+          const nameEs = data?.names?.find((name: any) => name.language.name === "es")?.name || ability.ability.name;
+          const descriptionEs =
+            data?.flavor_text_entries
+              ?.find((entry: any) => entry.language.name === "es")
+              ?.flavor_text?.replace(/\n/g, " ") || "";
 
-      return {
-        name: ability.ability.name,
-        url: ability.ability.url,
-        nameEs,
-        descriptionEs,
-      };
-    });
+          return {
+            name: ability.ability.name,
+            url: ability.ability.url,
+            nameEs,
+            descriptionEs,
+          };
+        },
+      );
 
-    abilities = await Promise.all(abilitiesPromises);
+      abilities = await Promise.all(abilitiesPromises);
+    }
+
+    return {
+      id: response.id,
+      name: response.name,
+      image: officialArtwork,
+      types,
+      abilities,
+    };
+  } catch {
+    return null;
   }
-
-  return {
-    id: response.id,
-    name: response.name,
-    image: officialArtwork,
-    types,
-    abilities,
-  };
 };
