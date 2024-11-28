23bfbc16ee53da3a0d2abaaf31435a43b28a2a0c
Luis Osio Chico <72286405+LOsioChico@users.noreply.github.com>
2024-11-28 14:17:13 -0500

    refactor: Remove StrictMode from render method

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,8 @@
 import "./index.css";
 
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router";
 
 import { router } from "./router";
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>,
-);
+createRoot(document.getElementById("root")!).render(<RouterProvider router={router} />);
