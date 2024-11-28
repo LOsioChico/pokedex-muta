8997a14e59e7fbabfef482fdb6db51a2e49803b4
Luis Osio Chico <72286405+LOsioChico@users.noreply.github.com>
2024-11-28 13:44:23 -0500

    feat: Add Pokemon type translations

---
+++ src/utils/pokemonTypeTranslations.ts
@@ -0,0 +1,22 @@
+import { PokemonType } from "../interfaces/Pokemon";
+
+export const POKEMON_TYPE_TRANSLATIONS: Record<PokemonType, string> = {
+  normal: "Normal",
+  fighting: "Lucha",
+  flying: "Volador",
+  poison: "Veneno",
+  ground: "Tierra",
+  rock: "Roca",
+  bug: "Bicho",
+  ghost: "Fantasma",
+  steel: "Acero",
+  fire: "Fuego",
+  water: "Agua",
+  grass: "Planta",
+  electric: "Eléctrico",
+  ice: "Hielo",
+  psychic: "Psíquico",
+  dragon: "Dragón",
+  dark: "Siniestro",
+  fairy: "Hada",
+};
