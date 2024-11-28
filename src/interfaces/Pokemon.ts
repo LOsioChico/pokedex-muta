c71e323d99e80e9ee99d51d90789417acab2d52a
Luis Osio Chico <72286405+LOsioChico@users.noreply.github.com>
2024-11-28 13:31:31 -0500

    refactor: Update Pokemon interface to use enum for types

--- src/interfaces/Pokemon.ts
+++ src/interfaces/Pokemon.ts
@@ -2,11 +2,31 @@ export interface Pokemon {
   id: number;
   name: string;
   image: string;
-  types: string[];
+  types: PokemonType[];
   stats: Stat[];
   height: number;
   weight: number;
 }
+
+export type PokemonType =
+  | "normal"
+  | "fire"
+  | "water"
+  | "electric"
+  | "grass"
+  | "ice"
+  | "fighting"
+  | "poison"
+  | "ground"
+  | "flying"
+  | "psychic"
+  | "bug"
+  | "rock"
+  | "ghost"
+  | "dragon"
+  | "dark"
+  | "steel"
+  | "fairy";
 
 interface Stat {
   name: string;
