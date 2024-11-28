a41731e12768d33ca0b656ec7e878ce8e4262f6c
Luis Osio Chico <72286405+LOsioChico@users.noreply.github.com>
2024-11-28 13:09:03 -0500

    feat: Add getPokemon function to fetch and format Pokemon data

--- src/api/pokeApi.ts
+++ src/api/pokeApi.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { Pokemon } from "../interfaces/Pokemon";
 
 export const pokeApi = axios.create({
   baseURL: "https://pokeapi.co/api/v2",
@@ -6,3 +7,30 @@ export const pokeApi = axios.create({
     "Content-Type": "application/json",
   },
 });
+
+export const getPokemon = async (id: string): Promise<Pokemon> => {
+  try {
+    const { data } = await pokeApi.get(`/pokemon/${id}`);
+
+    const officialArtwork = data.sprites.other["official-artwork"].front_default;
+    const types = data.types.map((type: { type: { name: string } }) => type.type.name);
+    const stats = data.stats.map((stat: { stat: { name: string }; base_stat: number }) => ({
+      name: stat.stat.name,
+      value: stat.base_stat,
+    }));
+
+    const pokemon: Pokemon = {
+      id: data.id,
+      name: data.name,
+      image: officialArtwork,
+      types,
+      stats,
+      height: data.height,
+      weight: data.weight,
+    };
+
+    return pokemon;
+  } catch {
+    throw new Error("Pokemon not found");
+  }
+};
--- src/hooks/usePokemon.ts
+++ src/hooks/usePokemon.ts
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
-import { pokeApi } from "../api/pokeApi";
 import { type Pokemon } from "../interfaces/Pokemon";
+import { getPokemon } from "../api/pokeApi";
 
 export const usePokemon = (id: string) => {
   const [pokemon, setPokemon] = useState<Pokemon | null>(null);
@@ -15,31 +15,4 @@ export const usePokemon = (id: string) => {
   }, [id]);
 
   return { pokemon, isLoading, error };
-};
-
-const getPokemon = async (id: string): Promise<Pokemon> => {
-  try {
-    const { data } = await pokeApi.get(`/pokemon/${id}`);
-
-    const officialArtwork = data.sprites.other["official-artwork"].front_default;
-    const types = data.types.map((type: { type: { name: string } }) => type.type.name);
-    const stats = data.stats.map((stat: { stat: { name: string }; base_stat: number }) => ({
-      name: stat.stat.name,
-      value: stat.base_stat,
-    }));
-
-    const pokemon: Pokemon = {
-      id: data.id,
-      name: data.name,
-      image: officialArtwork,
-      types,
-      stats,
-      height: data.height,
-      weight: data.weight,
-    };
-
-    return pokemon;
-  } catch {
-    throw new Error("Pokemon not found");
-  }
 };
