399af41cb08ed03f8ed9e9b4f7b4f0106478b035
Luis Osio Chico <72286405+LOsioChico@users.noreply.github.com>
2024-11-28 13:25:20 -0500

    feat: Add PokemonListResponse interface and update getPokemonList function

--- src/api/pokeApi.ts
+++ src/api/pokeApi.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { Pokemon } from "../interfaces/Pokemon";
 import { pokemonResponseMapper } from "../utils/pokemonResponseMapper";
+import { PokemonListResponse } from "../interfaces/PokemonListResponse";
 
 export const pokeApi = axios.create({
   baseURL: "https://pokeapi.co/api/v2",
@@ -19,6 +20,12 @@ export const getPokemon = async (id: string): Promise<Pokemon> => {
 };
 
 export const getPokemonList = async (limit: number = 20): Promise<Pokemon[]> => {
-  const { data } = await pokeApi.get(`/pokemon?limit=${limit}`);
-  return data.results.map(pokemonResponseMapper);
+  const { data: pokemonList } = await pokeApi.get<PokemonListResponse>(`/pokemon?limit=${limit}`);
+
+  const pokemonPromises = pokemonList.results.map(async (pokemonData) => {
+    const { data } = await pokeApi.get<Pokemon>(pokemonData.url);
+    return pokemonResponseMapper(data);
+  });
+
+  return Promise.all(pokemonPromises);
 };
